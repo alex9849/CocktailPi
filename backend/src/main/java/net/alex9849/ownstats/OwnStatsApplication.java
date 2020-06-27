c313b6246f4291521f57c707117dc1c14b1a8068
alex9849 <alexander@liggesmeyer.de>
2020-06-27 16:43:53 +0200

    travis test

--- backend/src/main/java/net/alex9849/ownstats/OwnStatsApplication.java
+++ backend/src/main/java/net/alex9849/ownstats/OwnStatsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OwnStatsApplication {
 
 	public static void main(String[] args) {
+		//
 		SpringApplication.run(OwnStatsApplication.class, args);
 	}
 
