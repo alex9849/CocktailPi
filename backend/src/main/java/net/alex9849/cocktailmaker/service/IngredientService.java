0babb1543c58e48a7b67c8578b83a462966a0fea
alex9849 <alexander@liggesmeyer.de>
2020-07-18 15:32:09 +0200

    Save changes (that don't work)

--- backend/src/main/java/net/alex9849/cocktailmaker/service/IngredientService.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/service/IngredientService.java
@@ -3,7 +3,9 @@ package net.alex9849.cocktailmaker.service;
 import net.alex9849.cocktailmaker.model.recipe.Ingredient;
 import net.alex9849.cocktailmaker.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class IngredientService {
 
     @Autowired
