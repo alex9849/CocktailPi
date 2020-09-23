7095ebe563ba0d101c5d367cf880a9fb5845e02e
alex9849 <alexander@liggesmeyer.de>
2020-09-23 15:55:33 +0200

    added unfinished view for the current cocktail

--- frontend/src/components/Circular-Cocktail-Progress.vue
+++ frontend/src/components/Circular-Cocktail-Progress.vue
@@ -17,7 +17,7 @@
     <q-dialog
       v-model="showDialog"
     >
-      <q-card q-card-section class="text-center" style="width: 500px">
+      <q-card q-card-section class="text-center" style="width: 100%">
         <q-card-section
           style="padding-bottom: 0"
         >
@@ -27,16 +27,83 @@
             :value="10"
             style="padding-bottom: 10px"
           />
-          <div v-if="hasCurrenlyPreparedCocktail">
+          <div v-if="hasCurrenlyCocktail">
+            <q-card
+              flat
+              style="background-color: #f3f3fa"
+              class="text-left"
+            >
+              <q-card-section>
+                <div
+                  class="row"
+                >
+                  <q-img
+                    :src="'/api/recipe/' + currentCocktail.recipe.id + '/image?nocache=' + new Date().getTime()"
+                    placeholder-src="../assets/cocktail-solid.png"
+                    :ratio="16/9"
+                    class="col rounded-borders"
+                    style="max-width: 225px; max-height: 180px"
+                  />
+                  <div class="col" style="padding-left: 10px; position: relative">
+                    <div class="row">
+                      <div class="col">
+                        <h5
+                          style="margin: 0; padding-bottom: 10px;"
+                        >
+                          <b>{{ currentCocktail.recipe.name}}</b>
+                        </h5>
+                      </div>
+                      <div class="col">
+                        <q-btn
 
+                        />
+                      </div>
+                    </div>
+                    <div class="row">
+                      {{ currentCocktail.recipe.shortDescription }}
+                    </div>
+
+                    <div class="row" style="position: absolute; bottom: 0; left: 0; right: 0; padding-inline: 10px">
+                      <div class="col"/>
+                      <div class="col" style="text-align: right; max-width: max-content">
+                        by {{ currentCocktail.recipe.owner.username }}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style="padding-top: 10px"
+                  class="row"
+                >
+                  <q-linear-progress
+                    :value="currentCocktail.progress / 100"
+                    stripe
+                    rounded
+                    color="green-5"
+                    size="20px"
+                  >
+                    <div class="absolute-full flex flex-center">
+                      <q-badge
+                        color="red-5"
+                        :label="currentCocktail.progress + '%'"
+                      />
+                    </div>
+                  </q-linear-progress>
+                </div>
+              </q-card-section>
+            </q-card>
           </div>
           <div v-else>
-            <p>Currently no cocktail gets prepared!</p>
-            <p>Go to "My recipes" or "Public recipes" to put one in order.</p>
+            <p>
+              Currently no cocktail gets prepared!
+            </p>
+            <p style="margin-bottom: 0">
+              Go to "My recipes" or "Public recipes" to put one in order.
+            </p>
           </div>
         </q-card-section>
         <q-card-actions
-          style="padding-top: 0; padding-bottom: 16px"
+          style="padding-top: 32px; padding-bottom: 16px"
           align="around"
         >
           <q-btn
@@ -66,8 +133,31 @@
       this.mdiTimerSandEmpty = mdiTimerSandEmpty;
     },
     computed: {
-      hasCurrenlyPreparedCocktail() {
-        return false;
+      hasCurrenlyCocktail() {
+        return true;
+      },
+      currentCocktail() {
+        return {
+          recipe: {
+            id: 12,
+            name: 'Jacky-Cola',
+            inPublic: true,
+            owner: {
+              id: 2,
+              username: 'Testuser'
+            },
+            description: 'Description',
+            shortDescription: 'Short description',
+            recipeIngredients: [],
+            tags: []
+          },
+          progress: 75,
+          user: {
+            id: 1,
+            username: 'Cocktailuser'
+          },
+          aborted: false
+        }
       }
     }
   }
@@ -77,6 +167,7 @@
   .actionable {
     border-radius: 4px;
   }
+
   .actionable:hover {
     box-shadow: inset 0 0 100px 100px rgba(255, 255, 255, 0.1);
   }
