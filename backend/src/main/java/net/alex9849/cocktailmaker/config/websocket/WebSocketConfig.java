5b2d80dd7c66265755b0718dd54d2f6f9ed8f985
alex9849 <alexander@liggesmeyer.de>
2020-10-03 20:23:52 +0200

    a small test

--- backend/src/main/java/net/alex9849/cocktailmaker/config/websocket/WebSocketConfig.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/websocket/WebSocketConfig.java
@@ -18,8 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/cocktailprogress");
-        registry.addEndpoint("/ws/cocktailprogress").withSockJS();
+        registry.addEndpoint("/ws/cocktailprogress").setAllowedOrigins("*");
+        registry.addEndpoint("/ws/cocktailprogress").setAllowedOrigins("*").withSockJS();
     }
 
 }
