d841ecb3b706f8f909c1d5f7da71316c8f6acea8
alex9849 <alexander@liggesmeyer.net>
2021-02-23 16:11:52 +0100

    fixed serveraddress

--- frontend/src/store/modules/auth/getters.js
+++ frontend/src/store/modules/auth/getters.js
@@ -1,8 +1,13 @@
+import { Platform } from 'quasar'
+
 export const isLoggedIn = state => state.status.loggedIn;
 export const getUser = state => state.status.user;
 export const getAuthToken = state => state.status.authToken;
 export const getServerAddress = state => state.status.serverAddress;
 export const getFormattedServerAddress = state => {
+  if(!Platform.is.cordova) {
+    return "";
+  }
   let address = state.status.serverAddress.toLowerCase();
   if(!/^((http:\/\/)|(https:\/\/)).+/.test(address)) {
     return "http://" + address;
--- frontend/src/store/modules/auth/state.js
+++ frontend/src/store/modules/auth/state.js
@@ -2,7 +2,7 @@ const authToken = JSON.parse(localStorage.getItem('authToken'));
 const user = JSON.parse(localStorage.getItem('user'));
 let serverAddress = JSON.parse(localStorage.getItem('serverAddress'));
 
-if(!!serverAddress) {
+if(serverAddress == undefined || serverAddress == null) {
   serverAddress = "";
 }
 
