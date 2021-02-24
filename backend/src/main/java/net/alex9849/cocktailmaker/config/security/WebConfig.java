5cf0e6dc23f8e1712e6266e067fda60cf6d6c029
alex9849 <alexander@liggesmeyer.net>
2021-02-24 10:26:01 +0100

    fixed ressource-handling

--- backend/src/main/java/net/alex9849/cocktailmaker/config/security/WebConfig.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/security/WebConfig.java
@@ -1,16 +1,24 @@
 package net.alex9849.cocktailmaker.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
