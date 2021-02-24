d3d141020bd6c78e2bcbd61edd5e8b5df4af78f1
alex9849 <alexander@liggesmeyer.net>
2021-02-24 11:03:23 +0100

    fixed cors filter

--- backend/src/main/java/net/alex9849/cocktailmaker/config/security/WebConfig.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/security/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE");
     }
 
     @Override
