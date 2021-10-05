bb98f23f60bcfdf6a91ea9bbc72e1ac042fc7192
alex9849 <alexander@liggesmeyer.net>
2021-10-05 16:32:09 +0200

    Added add button

--- frontend/src/pages/MyCollections.vue
+++ frontend/src/pages/MyCollections.vue
@@ -4,23 +4,37 @@
       <q-breadcrumbs-el label="My collections"/>
     </q-breadcrumbs>
     <h5>My collections</h5>
-    <div class="q-pa-md row items-start">
-      <div class="col-12 col-md-6 col-lg-4"
-           v-for="collection of collections"
-      >
-        <router-link :to="{name: 'collection', params: {id: collection.id}}">
+    <div class="q-pa-md row q-gutter-y-sm">
+        <router-link v-for="collection of collections"
+                     class="col-12 col-md-6 col-lg-4 d-flex items-stretch"
+                     :to="{name: 'collection', params: {id: collection.id}}"
+        >
           <c-collection-card
             :value="collection"
-            class="q-ma-xs"
+            class="q-ma-xs full-height"
           />
         </router-link>
-      </div>
+        <router-link class="col-12 col-md-6 col-lg-4 d-flex items-stretch"
+                     :to="{name: 'collection'}"
+        >
+          <q-card
+            :value="collection"
+            class="q-ma-xs full-height bg-grey-3"
+            flat
+            bordered
+          >
+            <q-card-actions align="center" class="full-height" >
+              <q-icon class="full-height" size="100px" :name="mdiPlusCircleOutline"></q-icon>
+            </q-card-actions>
+          </q-card>
+        </router-link>
     </div>
   </q-page>
 </template>
 
 <script>
 import CCollectionCard from "components/CCollectionCard";
+import {mdiPlusCircleOutline} from "@quasar/extras/mdi-v5";
 
 export default {
   name: "MyCollections",
@@ -58,6 +72,9 @@ export default {
         complete: true
       }]
     }
+  },
+  created() {
+    this.mdiPlusCircleOutline = mdiPlusCircleOutline;
   }
 }
 </script>
