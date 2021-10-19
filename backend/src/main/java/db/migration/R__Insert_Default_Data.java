d9652478c499c433716c19b1d48e33cdc22befc6
alex9849 <alexander@liggesmeyer.net>
2021-10-19 12:25:22 +0200

    created getOrCreateIngredient and getOrCreateCategory and Recipe methods

--- backend/src/main/java/db/migration/R__Insert_Default_Data.java
+++ backend/src/main/java/db/migration/R__Insert_Default_Data.java
@@ -1,5 +1,9 @@
 package db.migration;
 
+import net.alex9849.cocktailmaker.model.Category;
+import net.alex9849.cocktailmaker.model.recipe.Ingredient;
+import net.alex9849.cocktailmaker.model.recipe.Recipe;
+import net.alex9849.cocktailmaker.model.recipe.RecipeIngredient;
 import net.alex9849.cocktailmaker.model.user.ERole;
 import net.alex9849.cocktailmaker.model.user.User;
 import net.alex9849.cocktailmaker.service.CategoryService;
@@ -10,12 +14,18 @@ import net.alex9849.cocktailmaker.utils.SpringUtility;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class R__Insert_Default_Data extends BaseJavaMigration {
 
     private final UserService userService = SpringUtility.getBean(UserService.class);
     private final RecipeService recipeService = SpringUtility.getBean(RecipeService.class);
     private final CategoryService categoryService = SpringUtility.getBean(CategoryService.class);
     private final IngredientService ingredientService = SpringUtility.getBean(IngredientService.class);
+
+    private Map<String, Category> categoriesByName = null;
+    private Map<String, Ingredient> ingredientsByName = null;
 
     @Override
     public void migrate(Context context) throws Exception {
@@ -35,6 +45,42 @@ public class R__Insert_Default_Data extends BaseJavaMigration {
         defaultUser.setAccountNonLocked(true);
         defaultUser.setAuthority(ERole.ROLE_ADMIN);
         return userService.createUser(defaultUser);
+    }
+
+    private Recipe createRecipe(Recipe recipe) {
+        for(RecipeIngredient recipeIngredient : recipe.getRecipeIngredients()) {
+            Ingredient ingredient = getOrCreateIngredient(recipeIngredient.getIngredient());
+            recipeIngredient.setIngredient(ingredient);
+            recipeIngredient.setIngredientId(ingredient.getId());
+        }
+        for(int i = 0; i < recipe.getCategories().size(); i++) {
+            recipe.getCategories().set(i, getOrCreateCategory(recipe.getCategories().get(i)));
+        }
+        return recipeService.createRecipe(recipe);
+    }
+
+    private Ingredient getOrCreateIngredient(Ingredient searchIngredient) {
+        if(ingredientsByName == null) {
+            ingredientsByName = ingredientService.getIngredientByFilter(null, false)
+                    .stream().collect(Collectors.toMap(Ingredient::getName, x -> x));
+        }
+        if(!ingredientsByName.containsKey(searchIngredient.getName())) {
+            Ingredient createdIngredient = ingredientService.createIngredient(searchIngredient);
+            ingredientsByName.put(createdIngredient.getName(), createdIngredient);
+        }
+        return ingredientsByName.get(searchIngredient.getName());
+    }
+
+    private Category getOrCreateCategory(Category searchCategory) {
+        if(categoriesByName == null) {
+            categoriesByName = categoryService.getAllCategories().stream()
+                    .collect(Collectors.toMap(Category::getName, x -> x));
+        }
+        if(!categoriesByName.containsKey(searchCategory.getName())) {
+            Category createdCategory = categoryService.createCategory(searchCategory);
+            categoriesByName.put(createdCategory.getName(), createdCategory);
+        }
+        return categoriesByName.get(searchCategory.getName());
     }
 
 
