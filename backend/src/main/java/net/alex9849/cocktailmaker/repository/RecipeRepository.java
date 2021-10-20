fc1e2749b069c06f2aec95d1179439d6fde81665
alex9849 <alexander@liggesmeyer.net>
2021-10-20 16:13:11 +0200

    adjusted recipe-filter-sql-queries to new table structure

--- backend/src/main/java/net/alex9849/cocktailmaker/repository/RecipeRepository.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/repository/RecipeRepository.java
@@ -285,11 +285,14 @@ public class RecipeRepository extends JdbcDaoSupport {
 
     public Set<Long> getIdsOfFullyAutomaticallyFabricableRecipes() {
         return getJdbcTemplate().execute((ConnectionCallback<Set<Long>>) con -> {
-            PreparedStatement pstmt = con.prepareStatement("SELECT r.id AS id FROM recipes r " +
-                    "join recipe_ingredients ri on r.id = ri.recipe_id " +
-                    "join ingredients i on i.id = ri.ingredient_id AND i.dtype = 'AutomatedIngredient'" +
-                    "left join pumps p on i.id = p.current_ingredient_id " +
-                    "group by r.id having count(*) = count(p.id)");
+            PreparedStatement pstmt = con.prepareStatement("SELECT r.id AS id\n" +
+                    "FROM recipes r\n" +
+                    "         join production_steps ps on ps.recipe_id = r.id\n" +
+                    "         join production_step_ingredients psi on psi.recipe_id = ps.recipe_id and psi.\"order\" = ps.\"order\"\n" +
+                    "         join ingredients i on i.id = psi.ingredient_id AND i.dtype = 'AutomatedIngredient'\n" +
+                    "         left join pumps p on i.id = p.current_ingredient_id\n" +
+                    "group by r.id\n" +
+                    "having count(*) = count(p.id)");
             return DbUtils.executeGetIdsPstmt(pstmt);
         });
     }
@@ -297,15 +300,20 @@ public class RecipeRepository extends JdbcDaoSupport {
     public Set<Long> getIdsOfRecipesWithAllIngredientsOwnedOrOnPumps(long userId) {
         return getJdbcTemplate().execute((ConnectionCallback<Set<Long>>) con -> {
             PreparedStatement pstmt = con.prepareStatement(
-                    "SELECT recipeId from (SELECT r.id AS recipeId, CASE WHEN p.current_ingredient_id IS NOT NULL " +
-                            "OR uoi.ingredient_id IS NOT NULL THEN 1 ELSE 0 END AS owned\n" +
-                    "FROM recipes r\n" +
-                    "         join recipe_ingredients ri on r.id = ri.recipe_id\n" +
-                    "         join ingredients i on i.id = ri.ingredient_id\n" +
-                    "         left join pumps p on i.id = p.current_ingredient_id AND i.dtype = 'AutomatedIngredient'\n" +
-                    "         left join user_owned_ingredients uoi on i.id = uoi.ingredient_id AND uoi.user_id = ?) as sub\n" +
-                    "group by recipeId\n" +
-                    "having count(*) - sum(owned) = 0");
+                    "SELECT recipeId\n" +
+                            "from (SELECT r.id           AS recipeId,\n" +
+                            "             CASE\n" +
+                            "                 WHEN p.current_ingredient_id IS NOT NULL\n" +
+                            "                     OR uoi.ingredient_id IS NOT NULL THEN 1\n" +
+                            "                 ELSE 0 END AS owned\n" +
+                            "      FROM recipes r\n" +
+                            "               join production_steps ps on ps.recipe_id = r.id\n" +
+                            "               join production_step_ingredients psi on psi.recipe_id = ps.recipe_id and psi.\"order\" = ps.\"order\"\n" +
+                            "               join ingredients i on i.id = psi.ingredient_id\n" +
+                            "               left join pumps p on i.id = p.current_ingredient_id AND i.dtype = 'AutomatedIngredient'\n" +
+                            "               left join user_owned_ingredients uoi on i.id = uoi.ingredient_id AND uoi.user_id = ?) as sub\n" +
+                            "group by recipeId\n" +
+                            "having count(*) - sum(owned) = 0");
             pstmt.setLong(1, userId);
             return DbUtils.executeGetIdsPstmt(pstmt);
         });
