c7caa97911456ab6e38a0045b1a5605c363ea866
alex9849 <alexander@liggesmeyer.net>
2021-11-15 14:12:05 +0100

    vuex upgrade

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,41 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 import auth from './modules/auth/index'
 import cocktailProgress from './modules/cocktailprogress/index'
 import pumpLayout from './modules/pumplayout/index'
 import category from './modules/category/index'
 import bar from './modules/bar/index'
 
-// import example from './module-example'
-
-Vue.use(Vuex)
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-const store = new Vuex.Store({
-  modules: {
-    auth: auth,
-    cocktailProgress: cocktailProgress,
-    pumpLayout: pumpLayout,
-    category: category,
-    bar: bar
-  },
-
-  // enable strict mode (adds overhead!)
-  // for dev mode only
-  strict: process.env.DEV
-})
-
 export default function (/* { ssrContext } */) {
-  return store
-}
+  const Store = createStore({
+    modules: {
+      auth: auth,
+      cocktailProgress: cocktailProgress,
+      pumpLayout: pumpLayout,
+      category: category,
+      bar: bar
+    },
 
-export {
-  store
+    // enable strict mode (adds overhead!)
+    // for dev mode only
+    strict: process.env.DEV
+  })
+
+  return Store
 }
