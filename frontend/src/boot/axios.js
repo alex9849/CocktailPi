f717a72c7dd1947200e1c6dcf8dde2c2be05a3d8
alex9849 <alexander@liggesmeyer.net>
2021-11-15 14:18:00 +0100

    remove Vue.set uses

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,13 +1,13 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 import authHeader from 'src/services/auth-header'
-import { store } from '../store'
+import store from '../store'
 
 export default boot(({ Vue }) => {
   axios.defaults.baseURL = window.location.origin
   axios.interceptors.request.use(cfg => {
     cfg.headers.Authorization = authHeader()
-    cfg.baseURL = store.getters['auth/getFormattedServerAddress']
+    cfg.baseURL = store().getters['auth/getFormattedServerAddress']
     return cfg
   })
   Vue.prototype.globalProperties.$axios = axios
