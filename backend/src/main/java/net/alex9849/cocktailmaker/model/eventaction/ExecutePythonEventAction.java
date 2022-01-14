34ce0f613257526e9eabd17a5aecb21feb60f5f6
alex9849 <alexander@liggesmeyer.net>
2022-01-14 16:54:58 +0100

    Added logging

--- backend/src/main/java/net/alex9849/cocktailmaker/model/eventaction/ExecutePythonEventAction.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/model/eventaction/ExecutePythonEventAction.java
@@ -30,6 +30,12 @@ public class ExecutePythonEventAction extends FileEventAction {
             reader.close();
 
             process = Runtime.getRuntime().exec("python " + file.getAbsolutePath());
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(process.getInputStream()));
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
             process.waitFor();
             file.delete();
         } catch (InterruptedException e) {
