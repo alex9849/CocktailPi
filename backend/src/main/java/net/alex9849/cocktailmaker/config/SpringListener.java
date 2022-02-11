2af9561a0574a7c70391afec975120148fc1f14c
alex9849 <alexander@liggesmeyer.net>
2022-02-11 17:48:17 +0100

    moved flyway migration after application startup

--- backend/src/main/java/net/alex9849/cocktailmaker/config/SpringListener.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/SpringListener.java
@@ -5,8 +5,10 @@ import net.alex9849.cocktailmaker.service.EventService;
 import net.alex9849.cocktailmaker.service.PumpService;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -23,9 +25,13 @@ public class SpringListener {
     private Flyway flyway;
 
     @EventListener
-    public void handleContextStart(ApplicationReadyEvent event) {
+    public void handleApplicationPrepared(ContextRefreshedEvent event) {
         flyway.migrate();
         pumpService.turnAllPumpsOff();
+    }
+
+    @EventListener
+    public void handleContextStart(ApplicationReadyEvent event) {
         eventService.triggerActions(EventTrigger.APPLICATION_STARTUP);
     }
 }
