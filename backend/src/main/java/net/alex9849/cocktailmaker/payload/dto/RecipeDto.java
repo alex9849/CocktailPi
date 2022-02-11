cf64981a869303623544872b23040b5a5a252c4b
alex9849 <alexander@liggesmeyer.net>
2022-02-11 22:41:58 +0100

    lombok detailed dto

--- backend/src/main/java/net/alex9849/cocktailmaker/payload/dto/RecipeDto.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/payload/dto/RecipeDto.java
@@ -1,31 +1,40 @@
 package net.alex9849.cocktailmaker.payload.dto;
 
+import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Value;
+import lombok.NoArgsConstructor;
+import net.alex9849.cocktailmaker.model.recipe.Recipe;
 import net.alex9849.cocktailmaker.payload.dto.category.CategoryDto;
 import net.alex9849.cocktailmaker.payload.dto.recipe.IngredientDto;
 import net.alex9849.cocktailmaker.payload.dto.recipe.ProductionStepDto;
+import org.springframework.beans.BeanUtils;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public enum RecipeDto {;
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RecipeDto {
     private interface Id { long getId(); }
     private interface Name { @NotNull @Size(min = 1, max = 30) String getName(); }
+    private interface Description { @NotNull @Size(min = 1, max = 3000) String getDescription(); }
+    private interface ProductionSteps { @NotNull @NotEmpty List<ProductionStepDto> getProductionSteps(); }
+    private interface Categories { @NotNull Set<CategoryDto> getCategories(); }
+    private interface DefaultAmountToFill { @NotNull @Min(50) long getDefaultAmountToFill(); }
+
     private interface OwnerName { String getOwnerName(); }
-    private interface OwnerId { long getOwnerId(); }
-    private interface Description { @NotNull @Size(min = 0, max = 3000) String getDescription(); }
-    private interface ProductionSteps { @NotNull List<ProductionStepDto> getProductionSteps(); }
     private interface HasImage { boolean isHasImage(); }
     private interface Ingredients { Set<IngredientDto> getIngredients(); };
-    private interface Categories { @NotNull Set<CategoryDto> getCategories(); }
-    private interface DefaultAmountToFill { @NotNull long getDefaultAmountToFill(); }
     private interface LastUpdate { Date getLastUpdate(); }
+    private interface OwnerId { long getOwnerId(); }
 
-    public enum Request {;
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Request {
         @Data
         public static class Create implements Name, OwnerId, Description, ProductionSteps, Categories, DefaultAmountToFill {
             String name;
@@ -37,19 +46,36 @@ public enum RecipeDto {;
         }
     }
 
-    public enum Response {;
-        @Value public static class Detailed implements Name, OwnerId, Description, ProductionSteps, Categories, DefaultAmountToFill {
-            long id;
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class Response {
+
+        @Data
+        public static class Detailed implements Name, OwnerId, Description, ProductionSteps, Categories,
+                HasImage, DefaultAmountToFill, LastUpdate {
+            final long id;
             String name;
             long ownerId;
             String description;
             List<ProductionStepDto> productionSteps;
             Set<CategoryDto> categories;
+            boolean hasImage;
             long defaultAmountToFill;
+            Date lastUpdate;
+
+            public static Detailed fromEntity(Recipe recipe) {
+                Detailed dto = new Detailed(recipe.getId());
+                BeanUtils.copyProperties(recipe, dto);
+                dto.productionSteps = recipe.getProductionSteps().stream()
+                        .map(ProductionStepDto::toDto).collect(Collectors.toList());
+                dto.categories = recipe.getCategories().stream().map(CategoryDto::new)
+                        .collect(Collectors.toSet());
+                return dto;
+            }
         }
 
-        @Value public static class SearchResult implements Id, Name, OwnerName, Description, HasImage, Ingredients {
-            long id;
+        @Data
+        public static class SearchResult implements Id, Name, OwnerName, Description, HasImage, Ingredients {
+            final long id;
             String name;
             String ownerName;
             String description;
@@ -57,5 +83,4 @@ public enum RecipeDto {;
             Set<IngredientDto> ingredients;
         }
     }
-
 }
