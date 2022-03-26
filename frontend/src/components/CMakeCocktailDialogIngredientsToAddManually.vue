392c73e498bad29b98e6c0d812c9f870a8b5ba29
alex9849 <alexander@liggesmeyer.net>
2022-03-26 15:56:41 +0100

    Added CQHeadlinedCard.vue

--- frontend/src/components/CMakeCocktailDialogIngredientsToAddManually.vue
+++ frontend/src/components/CMakeCocktailDialogIngredientsToAddManually.vue
@@ -1,69 +1,36 @@
 <template>
-  <q-card flat
-          bordered
-          :class="cardClass"
+  <c-q-headlined-card
+    :card-class="cardClass"
+    :headline="headline"
+    :icon="icon"
+    :icon-background-class="iconBackgroundClass"
+    :icon-class="iconClass"
   >
-    <q-card-section
-      horizontal
-      v-if="unassignedIngredients.length !== 0"
-    >
-      <div class="flex items-center col-auto"
-           v-if="!$q.platform.is.mobile"
-      >
-        <q-icon :name="mdiAlertOutline"
-                size="xl"
-                class="text-orange-14 q-pa-xs"
-        ></q-icon>
-      </div>
-      <q-separator vertical
-                   v-if="!$q.platform.is.mobile"
-      />
-      <div class="col">
-        <div class="q-pa-sm">
-          The following ingredients have to get added manually or are not assigned to pumps. You will be asked to
-          add them during the production progress:
-        </div>
-        <q-separator></q-separator>
-        <ul style="text-align: start">
-          <li v-for="ingredient in unassignedIngredients" :key="ingredient.id">
-            {{ ingredient.name }}
-            <q-chip :color="ingredient.inBar? 'green-4' : 'red-4'"
-                    dense
-                    square
-                    :ripple="false"
-            >
-              <div v-if="ingredient.inBar">in bar</div>
-              <div v-else>not in bar</div>
-            </q-chip>
-          </li>
-        </ul>
-      </div>
-    </q-card-section>
-    <q-card-section
-      horizontal
-      v-else
-    >
-      <div class="flex items-center col-auto bg-light-green-14"
-      >
-        <q-icon :name="mdiCheck"
-                size="xl"
-                class="text-white q-pa-xs"
-        ></q-icon>
-      </div>
-      <q-separator vertical
-      />
-      <div class="col flex justify-center items-center">
-        <p class="q-pa-sm" style="text-wrap: normal">All ingredients assigned to pumps! Cocktail can be produced fully automatic!</p>
-      </div>
-    </q-card-section>
-  </q-card>
+    <template v-slot:content v-if="!isFulfilled">
+      <ul style="text-align: start">
+        <li v-for="ingredient in unassignedIngredients" :key="ingredient.id">
+          {{ ingredient.name }}
+          <q-chip :color="ingredient.inBar? 'green-4' : 'red-4'"
+                  dense
+                  square
+                  :ripple="false"
+          >
+            <div v-if="ingredient.inBar">in bar</div>
+            <div v-else>not in bar</div>
+          </q-chip>
+        </li>
+      </ul>
+    </template>
+  </c-q-headlined-card>
 </template>
 
 <script>
-import { mdiAlertOutline, mdiCheckOutline, mdiCheck } from '@quasar/extras/mdi-v5'
+import { mdiAlertOutline, mdiCheck, mdiCheckOutline } from '@quasar/extras/mdi-v5'
+import CQHeadlinedCard from 'components/CQHeadlinedCard'
 
 export default {
   name: 'CMakeCocktailDialogIngredientsToAddManually',
+  components: { CQHeadlinedCard },
   props: {
     unassignedIngredients: {
       type: Array,
@@ -73,16 +40,35 @@ export default {
   setup () {
     return {
       mdiAlertOutline: mdiAlertOutline,
-      mdiCheckOutline: mdiCheckOutline,
       mdiCheck: mdiCheck
     }
   },
   computed: {
+    isFulfilled () {
+      return this.unassignedIngredients.length === 0
+    },
     cardClass () {
       return {
-        'bg-warning': this.unassignedIngredients.length !== 0,
-        'bg-info': this.unassignedIngredients.length === 0
+        'bg-warning': !this.isFulfilled,
+        'bg-info': this.isFulfilled
       }
+    },
+    headline () {
+      if (this.isFulfilled) {
+        return 'All ingredients assigned to pumps! Cocktail can be produced fully automatic!'
+      } else {
+        return 'The following ingredients have to get added manually or are not assigned to pumps. ' +
+          'You will be asked to add them during the production progress:'
+      }
+    },
+    iconClass () {
+      return this.isFulfilled ? 'text-white' : 'text-orange-14'
+    },
+    iconBackgroundClass () {
+      return this.isFulfilled ? 'bg-light-green-14' : 'bg-warning'
+    },
+    icon () {
+      return this.isFulfilled ? mdiCheck : mdiAlertOutline
     }
   }
 }
---
+++ frontend/src/components/CQHeadlinedCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-card flat
+          bordered
+          :class="cardClass"
+  >
+    <q-card-section
+      horizontal
+    >
+      <div
+        class="flex items-center col-auto"
+        :class="iconBackgroundClass"
+      >
+        <q-icon :name="icon"
+                size="xl"
+                class="q-pa-xs"
+                :class="iconClass"
+        ></q-icon>
+      </div>
+      <q-separator vertical />
+      <div class="col flex justify-center items-center">
+        <div class="col">
+          <div class="row flex justify-center items-center self-center">
+            <p class="q-pa-sm">{{ headline }}</p>
+          </div>
+          <q-separator v-if="$slots.content" />
+          <div class="row">
+            <slot name="content"/>
+          </div>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'CQHeadlinedCard',
+  props: {
+    icon: null,
+    iconClass: null,
+    iconBackgroundClass: null,
+    cardClass: null,
+    headline: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
