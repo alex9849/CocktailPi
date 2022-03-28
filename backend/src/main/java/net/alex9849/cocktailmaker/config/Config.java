fbe6a64cdcb75e5ef85f266e9398c2924f039d4c
alex9849 <alexander@liggesmeyer.net>
2022-03-28 11:23:09 +0200

    Added database startup delay

--- backend/src/main/java/net/alex9849/cocktailmaker/config/Config.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/Config.java
@@ -1,10 +1,17 @@
 package net.alex9849.cocktailmaker.config;
 
 import net.alex9849.cocktailmaker.iface.IGpioController;
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableTransactionManagement
@@ -29,4 +36,31 @@ public class Config {
         }
         return gpioController;
     }
+
+    @Bean
+    public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+        var dsv = new DatabaseStartupValidator();
+        dsv.setDataSource(dataSource);
+        dsv.setTimeout(120);
+        dsv.setInterval(7);
+        return dsv;
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+        return bf -> {
+            // Let beans that need the database depend on the DatabaseStartupValidator
+            // like the JPA EntityManagerFactory or Flyway
+            String[] flyway = bf.getBeanNamesForType(Flyway.class);
+            Stream.of(flyway)
+                    .map(bf::getBeanDefinition)
+                    .forEach(it -> it.setDependsOn("databaseStartupValidator"));
+
+            String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+            Stream.of(jpa)
+                    .map(bf::getBeanDefinition)
+                    .forEach(it -> it.setDependsOn("databaseStartupValidator"));
+        };
+    }
+
 }
