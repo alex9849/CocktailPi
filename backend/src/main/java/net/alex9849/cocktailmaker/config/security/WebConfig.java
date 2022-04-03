661e0aaee54560fd2f392fedd0749a79f94ec814
alex9849 <alexander@liggesmeyer.net>
2022-04-03 12:28:58 +0200

    Added new urlforwarder

--- backend/src/main/java/net/alex9849/cocktailmaker/config/security/WebConfig.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/security/WebConfig.java
@@ -4,22 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/" };
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
