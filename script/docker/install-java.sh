6d22d22c8d56ce1d79c30c2fc4e70bbb050520fa
alex9849 <alexander@liggesmeyer.net>
2022-04-05 20:11:51 +0200

    updated deploy workflow

--- script/docker/install-java.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p /usr/lib/jvm
-cd /usr/lib/jvm || exit
-wget https://cdn.azul.com/zulu-embedded/bin/zulu11.41.75-ca-jdk11.0.8-linux_aarch32hf.tar.gz
-tar -xzvf zulu11.41.75-ca-jdk11.0.8-linux_aarch32hf.tar.gz
-rm zulu11.41.75-ca-jdk11.0.8-linux_aarch32hf.tar.gz
-update-alternatives --install /usr/bin/java java /usr/lib/jvm/zulu11.41.75-ca-jdk11.0.8-linux_aarch32hf/bin/java 1
-update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/zulu11.41.75-ca-jdk11.0.8-linux_aarch32hf/bin/javac 1
