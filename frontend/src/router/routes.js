f797c5abb5b38d602fb5b99d0e0eb6f01ffdc49b
alex9849 <alexander@liggesmeyer.net>
2022-08-22 11:45:51 +0200

    Added FullLayout and LoggedInLayout

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -29,83 +29,87 @@ const routes = [
     redirect: { name: 'login' },
     beforeEnter: redirectIfNotAuthenticated,
     children: [{
-      path: 'dashboard',
-      component: () => import('pages/Dashboard'),
-      name: 'dashboard'
-    }, {
-      path: 'bar',
-      component: () => import('pages/Bar'),
-      name: 'bar'
-    }, {
-      path: 'collection/own',
-      component: () => import('pages/MyCollections'),
-      name: 'mycollections'
-    }, {
-      path: 'collection/:id',
-      component: () => import('pages/Collection'),
-      name: 'collection'
-    }, {
-      path: 'user/profile',
-      component: () => import('pages/Profile'),
-      name: 'myprofile'
-    }, {
-      path: 'recipe/add',
-      component: () => import('pages/RecipeAdd'),
-      name: 'recipeadd'
-    }, {
-      path: 'recipe/own',
-      component: () => import('pages/RecipesOwn'),
-      name: 'myrecipes'
-    }, {
-      path: 'recipe/public',
-      component: () => import('pages/RecipesPublic'),
-      name: 'publicrecipes'
-    }, {
-      path: 'recipe/public/category/:cid',
-      component: () => import('pages/RecipesPublic'),
-      name: 'publiccategoryrecipes'
-    }, {
-      path: 'recipe/:id',
-      component: () => import('pages/RecipeDetails'),
-      name: 'recipedetails'
-    }, {
-      path: 'recipe/:id/order',
-      component: () => import('pages/RecipeDetails'),
-      name: 'recipeorder'
-    }, {
-      path: 'recipe/:id/edit',
-      component: () => import('pages/RecipeEdit'),
-      name: 'recipeedit'
-    }, {
-      path: 'admin/ingredient',
-      component: () => import('pages/IngredientManagement'),
-      name: 'ingredientmanagement'
-    }, {
-      path: 'admin/usermanagement',
-      component: () => import('pages/UserManagement'),
-      name: 'usermanagement'
-    }, {
-      path: 'admin/usermanagement/create',
-      component: () => import('pages/UserCreator'),
-      name: 'usercreator'
-    }, {
-      path: 'admin/usermanagement/:userId/edit',
-      component: () => import('pages/UserEditor'),
-      name: 'usereditor'
-    }, {
-      path: 'admin/pumpmanagement',
-      component: () => import('pages/PumpManagement'),
-      name: 'pumpmanagement'
-    }, {
-      path: 'admin/categorymanagement',
-      component: () => import('pages/CategoryManagement'),
-      name: 'categorymanagement'
-    }, {
-      path: 'admin/eventmanagement',
-      component: () => import('pages/EventManagement'),
-      name: 'eventmanagement'
-    }
-    ]
+      path: '/',
+      component: () => import('layouts/FullLayout.vue'),
+      children: [{
+        path: 'dashboard',
+        component: () => import('pages/Dashboard'),
+        name: 'dashboard'
+      }, {
+        path: 'bar',
+        component: () => import('pages/Bar'),
+        name: 'bar'
+      }, {
+        path: 'collection/own',
+        component: () => import('pages/MyCollections'),
+        name: 'mycollections'
+      }, {
+        path: 'collection/:id',
+        component: () => import('pages/Collection'),
+        name: 'collection'
+      }, {
+        path: 'user/profile',
+        component: () => import('pages/Profile'),
+        name: 'myprofile'
+      }, {
+        path: 'recipe/add',
+        component: () => import('pages/RecipeAdd'),
+        name: 'recipeadd'
+      }, {
+        path: 'recipe/own',
+        component: () => import('pages/RecipesOwn'),
+        name: 'myrecipes'
+      }, {
+        path: 'recipe/public',
+        component: () => import('pages/RecipesPublic'),
+        name: 'publicrecipes'
+      }, {
+        path: 'recipe/public/category/:cid',
+        component: () => import('pages/RecipesPublic'),
+        name: 'publiccategoryrecipes'
+      }, {
+        path: 'recipe/:id',
+        component: () => import('pages/RecipeDetails'),
+        name: 'recipedetails'
+      }, {
+        path: 'recipe/:id/order',
+        component: () => import('pages/RecipeDetails'),
+        name: 'recipeorder'
+      }, {
+        path: 'recipe/:id/edit',
+        component: () => import('pages/RecipeEdit'),
+        name: 'recipeedit'
+      }, {
+        path: 'admin/ingredient',
+        component: () => import('pages/IngredientManagement'),
+        name: 'ingredientmanagement'
+      }, {
+        path: 'admin/usermanagement',
+        component: () => import('pages/UserManagement'),
+        name: 'usermanagement'
+      }, {
+        path: 'admin/usermanagement/create',
+        component: () => import('pages/UserCreator'),
+        name: 'usercreator'
+      }, {
+        path: 'admin/usermanagement/:userId/edit',
+        component: () => import('pages/UserEditor'),
+        name: 'usereditor'
+      }, {
+        path: 'admin/pumpmanagement',
+        component: () => import('pages/PumpManagement'),
+        name: 'pumpmanagement'
+      }, {
+        path: 'admin/categorymanagement',
+        component: () => import('pages/CategoryManagement'),
+        name: 'categorymanagement'
+      }, {
+        path: 'admin/eventmanagement',
+        component: () => import('pages/EventManagement'),
+        name: 'eventmanagement'
+      }
+      ]
+    }]
   }, {
     path: '/login',
     component: () => import('layouts/EmptyLayout'),
