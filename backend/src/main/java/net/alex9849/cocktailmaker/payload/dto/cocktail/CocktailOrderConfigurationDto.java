d4fedb45c131d6d743f6781378b1a2ebe5d0ae81
alex9849 <alexander@liggesmeyer.net>
2022-10-10 11:41:27 +0200

    Added Customisations to Order Dto

--- backend/src/main/java/net/alex9849/cocktailmaker/payload/dto/cocktail/CocktailOrderConfigurationDto.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/payload/dto/cocktail/CocktailOrderConfigurationDto.java
@@ -11,13 +11,46 @@ import java.util.List;
 public class CocktailOrderConfigurationDto {
     public interface AmountToProduce { @Min(50) @Max(1000) Integer getAmountOrderedInMl(); }
     public interface ProductionStepReplacements { @NotNull() List<List<FeasibilityReportDto.IngredientGroupReplacementDto.Request.Create>> getProductionStepReplacements(); }
+    public interface Customisations { @NotNull() CustomisationsDto.Request.Create getCustomisations(); }
 
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     public static class Request {
         @Getter @Setter @EqualsAndHashCode
-        public static class Create implements AmountToProduce, ProductionStepReplacements {
+        public static class Create implements AmountToProduce, ProductionStepReplacements, Customisations {
             Integer amountOrderedInMl;
             List<List<FeasibilityReportDto.IngredientGroupReplacementDto.Request.Create>> productionStepReplacements;
+            CustomisationsDto.Request.Create customisations;
         }
     }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class CustomisationsDto {
+        public interface Boost { @Min(0) @Max(200) int getBoost(); }
+        public interface AdditionalIngredients { @NotNull() List<AdditionalIngredientDto.Request.Create> getAdditionalIngredients(); }
+
+        @NoArgsConstructor(access = AccessLevel.PRIVATE)
+        public static class Request {
+            @Getter @Setter @EqualsAndHashCode
+            public static class Create implements Boost, AdditionalIngredients {
+                int boost;
+                List<AdditionalIngredientDto.Request.Create> additionalIngredients;
+            }
+        }
+
+        @NoArgsConstructor(access = AccessLevel.PRIVATE)
+        public static class AdditionalIngredientDto {
+            private interface IngredientId { @NotNull long getIngredientId(); }
+            private interface Amount { @Min(1) int getAmount(); }
+
+            @NoArgsConstructor(access = AccessLevel.PRIVATE)
+            public static class Request {
+                @Getter @Setter @EqualsAndHashCode
+                public static class Create implements IngredientId, Amount {
+                    long ingredientId;
+                    int amount;
+                }
+            }
+        }
+    }
+
 }
