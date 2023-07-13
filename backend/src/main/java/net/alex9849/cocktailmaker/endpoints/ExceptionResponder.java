ad844bf8ae1b410451d82d5bacfa5ef348411155
alex9849 <alexander@liggesmeyer.net>
2023-07-13 18:27:00 +0200

    first update fields work

--- backend/src/main/java/net/alex9849/cocktailmaker/endpoints/ExceptionResponder.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/endpoints/ExceptionResponder.java
@@ -10,10 +10,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.Date;
 
 @ControllerAdvice
-public class ExceptionResponder extends ResponseEntityExceptionHandler {
+public class ExceptionResponder {
 
     @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
-    public ResponseEntity<ErrorDetails> handleException(RuntimeException ex, WebRequest request) {
+    public ResponseEntity<ErrorDetails> handleException(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
