3fda3ee177fbe11a5eb5a2f40f751424422583d1
alex9849 <alexander@liggesmeyer.net>
2023-08-20 18:59:31 +0200

    reconfigured sqlite

--- backend/src/main/java/net/alex9849/cocktailmaker/config/SpringListener.java
+++ backend/src/main/java/net/alex9849/cocktailmaker/config/SpringListener.java
@@ -33,8 +33,8 @@ public class SpringListener {
     @Autowired
     private SeedDataInserter seedDataInserter;
 
-    @PostConstruct
-    public void onPostConstruct() throws Exception {
+    @EventListener
+    public void handleContextRefreshed(ContextRefreshedEvent event) throws Exception {
         flyway.migrate();
         seedDataInserter.migrate();
         pumpService.postConstruct();
