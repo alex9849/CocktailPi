0dd402591b44b5e277900e465803be99b0576a65
alex9849 <alexander@liggesmeyer.net>
2023-09-09 20:55:04 +0200

    Added help disclaimer to GlassManagement.vue

--- frontend/src/components/CMakeCocktailDialogAmountToProduce.vue
+++ frontend/src/components/CMakeCocktailDialogAmountToProduce.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center q-col-gutter-sm">
     <q-select
       v-if="showGlassSelector"
       :model-value="selectedGlass"
