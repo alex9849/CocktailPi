f2c84a8d8e40a3b341c0f45e5c6d052e914b8f03
alex9849 <alexander@liggesmeyer.net>
2023-11-04 17:03:20 +0100

    added i18n translation lib

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -20,7 +20,8 @@ module.exports = function (/* ctx */) {
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
       'axios',
-      'store'
+      'store',
+      'i18n'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
