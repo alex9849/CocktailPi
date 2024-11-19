93e43bf0d1713bfd5f53441784315620d2eb4e43
alex9849 <alexander@liggesmeyer.net>
2024-11-19 19:39:59 +0100

    added status style for pump overview

---
+++ frontend/src/components/CPumpStatus.vue
@@ -0,0 +1,189 @@
+<template>
+  <q-card class="bg-card-body text-card-body">
+    <q-card-section class="q-pa-none">
+      <q-expansion-item
+        label="Status"
+        header-class="bg-card-header text-card-header text-weight-medium"
+        :default-opened="!$q.platform.is.mobile"
+      >
+        <template v-slot:default>
+          <q-separator/>
+          <q-card-section>
+            <div class="row q-col-gutter-sm justify-center">
+              <div class="col-12 col-md-4 col-lg-12">
+                <q-card
+                  flat
+                  bordered
+                  class="bg-card-item-group text-card-item-group full-height"
+                >
+                  <q-card-section class="q-py-xs bg-card-header text-card-header">
+                    <p class="text-weight-medium">Pumps</p>
+                  </q-card-section>
+                  <q-separator/>
+                  <q-card-section
+                    class="q-py-sm"
+                  >
+                    <table class="table-no-stripes">
+                      <tbody>
+                      <tr>
+                        <td>Pumps installed:</td>
+                        <td>
+                          <q-badge>0</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Ingredients installed:</td>
+                        <td>
+                          <q-badge>0</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Pumps running:</td>
+                        <td>
+                          <q-badge>0</q-badge>
+                        </td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </q-card-section>
+                </q-card>
+              </div>
+              <div class="col-12 col-md-4 col-lg-12">
+                <q-card
+                  flat
+                  bordered
+                  class="bg-card-item-group text-card-item-group full-height"
+                >
+                  <q-card-section
+                    class="q-py-xs q-pr-xs bg-cyan-1 row items-center bg-card-header text-card-header"
+                  >
+                    <div class="col">
+                      <p class="text-weight-medium">Reverse pumping</p>
+                    </div>
+                    <div class="col-shrink">
+                      <q-btn
+                        color="info"
+                        label="Configure"
+                        :icon="mdiPencilOutline"
+                        @click="$router.push({name: 'reversepumpsettings'})"
+                        dense
+                        no-caps
+                        size="sm"
+                      />
+                    </div>
+                  </q-card-section>
+                  <q-separator/>
+                  <q-card-section
+                    class="q-py-sm"
+                  >
+                    <table class="table-no-stripes">
+                      <tbody>
+                      <tr>
+                        <td>Status:</td>
+                        <td>
+                          <q-badge>enabled</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Dir-Pin:</td>
+                        <td>
+                          <q-badge>Local board / BCM 0</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Overshoot:</td>
+                        <td>
+                          <q-badge>10%</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Timer:</td>
+                        <td>
+                          <q-badge>5 min</q-badge>
+                        </td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </q-card-section>
+                </q-card>
+              </div>
+              <div class="col-12 col-md-4 col-lg-12">
+                <q-card
+                  flat
+                  bordered
+                  class="bg-card-item-group text-card-item-group full-height"
+                >
+                  <q-card-section
+                    class="q-py-xs q-pr-xs bg-cyan-1 row items-center bg-card-header text-card-header"
+                  >
+                    <div class="col">
+                      <p class="text-weight-medium">Load cell</p>
+                    </div>
+                    <div class="col-shrink">
+                      <q-btn
+                        color="info"
+                        label="Configure"
+                        :icon="mdiPencilOutline"
+                        dense
+                        no-caps
+                        size="sm"
+                      />
+                    </div>
+                  </q-card-section>
+                  <q-separator/>
+                  <q-card-section
+                    class="q-py-sm"
+                  >
+                    <table class="table-no-stripes">
+                      <tbody>
+                      <tr>
+                        <td>Status:</td>
+                        <td>
+                          <q-badge>enabled</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>CLK-Pin:</td>
+                        <td>
+                          <q-badge>Local board / BCM 0</q-badge>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>DT-Pin:</td>
+                        <td>
+                          <q-badge>Local board / BCM 1</q-badge>
+                        </td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </q-card-section>
+                </q-card>
+              </div>
+            </div>
+          </q-card-section>
+        </template>
+      </q-expansion-item>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { mdiPencilOutline } from '@quasar/extras/mdi-v5'
+
+export default {
+  name: 'CPumpStatus',
+  props: {
+    classed: {
+      type: String,
+      default: ''
+    }
+  },
+  created () {
+    this.mdiPencilOutline = mdiPencilOutline
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/pages/PumpManagement.vue
+++ frontend/src/pages/PumpManagement.vue
@@ -4,22 +4,6 @@
     <div class="q-gutter-sm">
       <TopButtonArranger>
         <q-btn
-          color="info"
-          label="Configure reverse pumping"
-          @click="$router.push({name: 'reversepumpsettings'})"
-          :icon="mdiPlusCircleOutline"
-          no-caps
-        />
-        <q-btn
-          color="info"
-          label="Configure load cell"
-          @click="showAddDialog = true"
-          :icon="mdiPlusCircleOutline"
-          no-caps
-        />
-      </TopButtonArranger>
-      <TopButtonArranger>
-        <q-btn
           color="positive"
           :label="$t('component.pump_mgmt.add_btn_label')"
           @click="showAddDialog = true"
@@ -42,28 +26,35 @@
         />
       </TopButtonArranger>
     </div>
-    <div class="row q-col-gutter-md q-mt-md">
-      <div
-        class="col-12 col-sm-6 col-lg-4"
-        v-bind:key="pump.id"
-        v-for="pump in pumps"
-      >
-        <c-pump-card
-          style="height: 100%"
-          :pump="pump"
-          show-detailed
-        />
+    <div class="row q-col-gutter-lg q-mt-md reverse">
+      <div class="col-lg-3 col-12">
+        <c-pump-status />
       </div>
-      <div
-        class="col-12"
-        v-if="pumps.length === 0"
-      >
-        <q-card flat bordered class="bg-card-header text-card-header">
-          <div class="row q-pa-md items-center q-gutter-sm">
-            <q-icon size="sm" :name="mdiAlert" />
-            <p class="">{{ $t('component.pump_mgmt.no_pumps_found') }}</p>
+      <div class="col-lg-9 col-12">
+        <div class="row q-col-gutter-sm">
+          <div
+            class="col-12 col-sm-6 col-lg-4"
+            v-bind:key="pump.id"
+            v-for="pump in pumps"
+          >
+            <c-pump-card
+              style="height: 100%"
+              :pump="pump"
+              show-detailed
+            />
           </div>
-        </q-card>
+          <div
+            class="col-12"
+            v-if="pumps.length === 0"
+          >
+            <q-card flat bordered class="bg-card-header text-card-header">
+              <div class="row q-pa-md items-center q-gutter-sm">
+                <q-icon size="sm" :name="mdiAlert" />
+                <p class="">{{ $t('component.pump_mgmt.no_pumps_found') }}</p>
+              </div>
+            </q-card>
+          </div>
+        </div>
       </div>
     </div>
     <c-pump-setup-type-selector
@@ -80,10 +71,11 @@ import CPumpSetupTypeSelector from 'components/pumpsetup/CPumpSetupTypeSelector.
 import { mdiAlert, mdiDelete, mdiPencilOutline, mdiPlay, mdiPlusCircleOutline, mdiStop } from '@quasar/extras/mdi-v5'
 import PumpService from 'src/services/pump.service'
 import { mapGetters } from 'vuex'
+import CPumpStatus from 'components/CPumpStatus.vue'
 
 export default {
   name: 'PumpManagement',
-  components: { CPumpSetupTypeSelector, CPumpCard, TopButtonArranger },
+  components: { CPumpStatus, CPumpSetupTypeSelector, CPumpCard, TopButtonArranger },
   created () {
     this.mdiDelete = mdiDelete
     this.mdiPencilOutline = mdiPencilOutline
