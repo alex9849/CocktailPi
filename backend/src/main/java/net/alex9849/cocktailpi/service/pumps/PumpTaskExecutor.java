eeda9c243a849a91de1637cd427506937e9ff675
alex9849 <alexander@liggesmeyer.net>
2025-07-02 20:06:17 +0200

    Implemented PumpTaskExecutor

--- backend/src/main/java/net/alex9849/cocktailpi/service/pumps/PumpTaskExecutor.java
+++ backend/src/main/java/net/alex9849/cocktailpi/service/pumps/PumpTaskExecutor.java
@@ -5,22 +5,24 @@ import net.alex9849.cocktailpi.model.pump.motortasks.PumpTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class PumpTaskExecutor extends Thread {
     private static PumpTaskExecutor instance;
-    private final ExecutorService liveTasksExecutor = Executors.newCachedThreadPool();
-    private List<List<PumpTask>> pumpTaskGroups = new ArrayList<>();
-    private int currentGroupIdx = 0;
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final List<List<PumpTask>> pumpTaskGroups = new ArrayList<>();
     private Integer powerLimit = null;
 
     private PumpTaskExecutor() {}
 
     public static PumpTaskExecutor getInstance() {
-        if(PumpTaskExecutor.instance == null) {
+        if (PumpTaskExecutor.instance == null) {
             PumpTaskExecutor.instance = new PumpTaskExecutor();
+            PumpTaskExecutor.instance.setDaemon(true);
+            PumpTaskExecutor.instance.start();
         }
         return PumpTaskExecutor.instance;
     }
@@ -33,30 +35,91 @@ public class PumpTaskExecutor extends Thread {
         this.powerLimit = null;
     }
 
-    public void deleteFinishedPumpTasks() {
+    public void cleanUpFinishedPumpTasks() {
+        for (List<PumpTask> pumpTasks : new ArrayList<>(pumpTaskGroups)) {
+            pumpTasks.removeIf(PumpTask::isFinished);
+        }
+        pumpTaskGroups.removeIf(List::isEmpty);
     }
 
 
     public void run() {
-        while (true) {
+        synchronized (pumpTaskGroups) {
+            int currentGroupIdx = 0;
+            try {
+                while (true) {
+                    long timeTillSuspend = 10_000;
 
+                    while (pumpTaskGroups.isEmpty()) {
+                        pumpTaskGroups.wait();
+                    }
+
+                    List<PumpTask> currentGroup = pumpTaskGroups.get(currentGroupIdx);
+
+                    boolean suspendGroup = false;
+                    long startWaitTime = System.currentTimeMillis();
+                    while (!suspendGroup) {
+                        for (PumpTask pumpTask : currentGroup) {
+                            pumpTask.signalStart();
+                        }
+                        pumpTaskGroups.wait(timeTillSuspend);
+                        suspendGroup = System.currentTimeMillis() - startWaitTime >= timeTillSuspend;
+                        cleanUpFinishedPumpTasks();
+                        suspendGroup &= pumpTaskGroups.size() > 1;
+                        if (currentGroup.isEmpty()) {
+                            break;
+                        }
+                        if (suspendGroup) {
+                            List<Callable<Void>> suspendTasks = new ArrayList<>();
+                            for (PumpTask pumpTask : currentGroup) {
+                                suspendTasks.add(() -> pumpTask.suspend());
+                            }
+                            try {
+                                List<Future<Void>> futures = executor.invokeAll(suspendTasks);
+                                for (Future<Void> future : futures) {
+                                    try {
+                                        future.get();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                    }
+                    if(pumpTaskGroups.isEmpty()) {
+                        currentGroupIdx = 0;
+                    } else {
+                        currentGroupIdx = (currentGroupIdx + 1) % pumpTaskGroups.size();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
 
-
-    public synchronized Future<?> submit(PumpTask task) {
-        for(List<PumpTask> group : pumpTaskGroups) {
-            int groupConsumption = group.stream().mapToInt(x -> x.getPump().getPowerConsumption()).sum();
-            if(groupConsumption + task.getPump().getPowerConsumption() > this.powerLimit) {
-                continue;
+    public Future<?> submit(PumpTask task) {
+        synchronized (this.pumpTaskGroups) {
+            for (List<PumpTask> group : pumpTaskGroups) {
+                if (this.powerLimit != null) {
+                    int groupConsumption = group.stream().mapToInt(x -> x.getPump().getPowerConsumption()).sum();
+                    if (groupConsumption + task.getPump().getPowerConsumption() > this.powerLimit) {
+                        continue;
+                    }
+                }
+                group.add(task);
+                this.pumpTaskGroups.notify();
+                return null;
             }
-            group.add(task);
+            List<PumpTask> newGroup = new ArrayList<>();
+            newGroup.add(task);
+            this.pumpTaskGroups.add(newGroup);
+            this.pumpTaskGroups.notify();
             return null;
         }
-        List<PumpTask> newGroup = new ArrayList<>();
-        newGroup.add(task);
-        this.pumpTaskGroups.add(newGroup);
-        return null;
     }
 }
