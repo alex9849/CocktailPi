cbe01e1c8bf16ab55c40410b3a618c6d5b193667
alex9849 <alexander@liggesmeyer.net>
2025-07-19 10:45:43 +0200

    migration sql

--- backend/src/main/resources/db/migration/V1.7.0__Glass_Detection.sql
+++
@@ -1,2 +0,0 @@
-alter table glasses
-    add column empty_weight INTEGER default null check (empty_weight > 0);
--- backend/src/main/resources/db/migration/V1.7.0__Glass_Detection_Power_Limit.sql
+++ backend/src/main/resources/db/migration/V1.7.0__Glass_Detection_Power_Limit.sql
@@ -1,2 +1,5 @@
 alter table pumps
     add column power_consumption INTEGER not null default 0 check (power_consumption >= 0);
+
+alter table glasses
+    add column empty_weight INTEGER default null check (empty_weight > 0);
