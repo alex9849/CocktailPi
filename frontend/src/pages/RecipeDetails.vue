7a0e9d3b85dc45915d3a7ad37342206047bba48a
alex9849 <alexander@liggesmeyer.net>
2025-08-17 21:28:04 +0200

    wip redesign of recipe details page

--- frontend/src/pages/RecipeDetails.vue
+++ frontend/src/pages/RecipeDetails.vue
@@ -1,122 +1,167 @@
 <template>
   <q-page class="page-content" padding>
-    <div class="row">
-      <div class="col">
-        <h5>{{ recipe.name }}</h5>
-      </div>
-    </div>
-    <div class="q-col-gutter-md">
-      <q-breadcrumbs separator="/" class="text-orange" active-color="secondary">
-        <q-breadcrumbs-el label="Recipes" :to="lastRecipeListRoute" :disable="!lastRecipeListRoute"/>
-        <q-breadcrumbs-el :label="recipe.name" />
-      </q-breadcrumbs>
-      <TopButtonArranger>
-        <q-btn
-          color="grey"
-          :to="{name: 'recipeedit', params: {id: recipe.id}}"
-          v-if="isAdminRole || (user?.id === recipe.ownerId && isRecipeCreatorRole)"
-        >
-          {{ $t('page.recipe_details.edit_btn_label') }}
-        </q-btn>
-        <q-btn
-          color="positive"
-          @click="showMakeCocktailDialog = true"
-        >
-          {{ $t('page.recipe_details.produce_btn_label') }}
-        </q-btn>
-        <q-btn
-          color="red"
-          @click="deleteDialog = true"
-          :loading="deleting"
-          v-if="isAdminRole || (user?.id === recipe.ownerId && isRecipeCreatorRole)"
-        >
-          {{ $t('page.recipe_details.delete_btn_label') }}
-        </q-btn>
-      </TopButtonArranger>
-      <div class="row q-col-gutter-md">
-        <div class="col-12 col-sm-6">
-          <div>
-            <q-img
-              v-if="recipe.hasImage"
-              :ratio="16/9"
-              :src="$store.getters['auth/getFormattedServerAddress'] + '/api/recipe/' + recipe.id + '/image?timestamp=' + recipe.lastUpdate.getTime() + '&width=1500'"
-              class="rounded-borders shadow-2"
-              placeholder-src="~assets/cocktail-solid.png"
-            />
-            <q-img
-              v-else
-              placeholder-src="~assets/cocktail-solid.png"
-              src="~assets/cocktail-solid.png"
-              :ratio="16/9"
-              class="rounded-borders shadow-2"
-            />
-          </div>
-        </div>
-        <div class="col-12 col-sm-6">
-          <div>
+    <div class="row q-col-gutter-xl">
+      <!-- Linke Spalte: Bild, Zutaten, Glas -->
+      <div class="col-12 col-md-5">
+        <q-card class="shadow-2 bg-card-body text-card-body q-mb-md">
+          <q-img
+            v-if="recipe.hasImage"
+            :src="$store.getters['auth/getFormattedServerAddress'] + '/api/recipe/' + recipe.id + '/image?timestamp=' + recipe.lastUpdate.getTime() + '&width=1500'"
+            :ratio="16/9"
+            class="rounded-borders"
+            placeholder-src="~assets/cocktail-solid.png"
+          />
+          <q-img
+            v-else
+            src="~assets/cocktail-solid.png"
+            :ratio="16/9"
+            class="rounded-borders"
+          />
+        </q-card>
+        <q-card class="shadow-2 bg-card-body text-card-body q-mb-md">
+          <q-card-section>
+            <div class="text-h6 q-mb-sm">
+              <q-icon name="restaurant_menu" class="q-mr-xs" /> Zutaten
+            </div>
             <ingredient-list
               big
               alternateRowColors
-              class="shadow-2"
               :background-color="color.cardBody"
               v-model:model-value="recipe.productionSteps"
             />
-          </div>
-        </div>
-      </div>
-      <div>
-        <q-card
-          bordered
-          class="shadow-2 bg-card-body text-card-body"
-        >
+          </q-card-section>
+        </q-card>
+        <q-card class="shadow-2 bg-card-body text-card-body">
           <q-card-section>
-            <div style="display: block ruby">
-              <b>{{ $t('page.recipe_details.categories_headline') }}</b>
-              <div
-                v-if="recipe.categories.length !== 0"
-              >
-                <q-badge
-                  class="q-mx-xs"
-                  v-for="category in recipe.categories"
-                  :key="category.id"
-                  :label="category.name"
-                />
-              </div>
-              <div
-                v-else
-              >
-                {{ $t('page.recipe_details.none') }}
-              </div>
+            <div class="text-h6 q-mb-xs">
+              <q-icon name="wine_bar" class="q-mr-xs" /> Glas
             </div>
-            <p>
-              <b>
-                {{ $t('page.recipe_details.default_glass_headline') }}
-              </b>
+            <div>
               {{ printGlass }}
-            </p>
+            </div>
           </q-card-section>
         </q-card>
       </div>
-      <div
-        v-if="recipe.description"
-        class="row"
-      >
-        <div class="col">
-          <q-card
-            bordered
-            class="shadow-2 bg-card-body text-card-body"
-            style="min-height: 100px"
-          >
-            <q-card-section>
-              <b>{{ $t('page.recipe_details.description_headline') }}</b>
-              <div style="min-width: 200px; white-space: pre-line" class="col">
-                {{ recipe.description }}
-              </div>
-            </q-card-section>
-          </q-card>
+      <!-- Rechte Spalte: Name, Info-Card, Beschreibung, Aktionen -->
+      <div class="col-12 col-md-7">
+        <div class="row items-center q-mb-md">
+          <div class="col">
+            <div class="text-h4">{{ recipe.name }}</div>
+            <div class="text-subtitle2 text-grey">
+              {{ $t('page.recipe_details.by') }} {{ recipe.owner?.username || 'Unbekannt' }}
+            </div>
+          </div>
+          <div class="col-auto">
+            <TopButtonArranger>
+              <q-btn
+                color="grey"
+                :to="{name: 'recipeedit', params: {id: recipe.id}}"
+                v-if="isAdminRole || (user?.id === recipe.ownerId && isRecipeCreatorRole)"
+                icon="edit"
+                flat
+              >
+                {{ $t('page.recipe_details.edit_btn_label') }}
+              </q-btn>
+              <q-btn
+                color="positive"
+                @click="showMakeCocktailDialog = true"
+                icon="play_arrow"
+                flat
+              >
+                {{ $t('page.recipe_details.produce_btn_label') }}
+              </q-btn>
+              <q-btn
+                color="red"
+                @click="deleteDialog = true"
+                :loading="deleting"
+                v-if="isAdminRole || (user?.id === recipe.ownerId && isRecipeCreatorRole)"
+                icon="delete"
+                flat
+              >
+                {{ $t('page.recipe_details.delete_btn_label') }}
+              </q-btn>
+            </TopButtonArranger>
+          </div>
         </div>
+        <q-card class="shadow-2 bg-card-body text-card-body q-mb-md">
+          <q-card-section>
+            <div class="text-h6 q-mb-xs">
+              <q-icon name="description" class="q-mr-xs" /> {{ $t('page.recipe_details.description_headline') }}
+            </div>
+            <div style="white-space: pre-line">{{ recipe.description }}</div>
+          </q-card-section>
+        </q-card>
+
+        <div class="row q-col-gutter-md q-mb-md">
+          <div class="col-12 col-sm-6 col-md-4">
+            <q-card class="shadow-1 bg-card-body text-card-body">
+              <q-card-section>
+                <div class="text-caption text-grey-7 q-mb-xs">
+                  <q-icon name="bolt" class="q-mr-xs" /> Boostbar
+                </div>
+                <div class="flex items-center" style="min-height: 32px;">
+                  <q-icon name="bolt" color="orange" size="32px" v-if="recipe.boostable" />
+                  <span class="text-h6 text-orange" v-if="recipe.boostable">Boostbar verfügbar</span>
+                  <span class="text-h6 text-grey-6" v-else>Keine Boostbar</span>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <q-card class="shadow-1 bg-card-body text-card-body">
+              <q-card-section>
+                <div class="text-caption text-grey-7 q-mb-xs">
+                  <q-icon name="local_bar" class="q-mr-xs" /> Alkoholgehalt
+                </div>
+                <div class="flex items-center" style="min-height: 32px;">
+                  <q-icon name="local_bar" color="teal" size="32px" v-if="recipe.minAlcoholContent" />
+                  <span class="text-h6 text-teal" v-if="recipe.minAlcoholContent">{{ recipe.minAlcoholContent }}% Vol.</span>
+                  <span class="text-h6 text-grey-6" v-else>Kein Alkohol</span>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-12 col-sm-12 col-md-4">
+            <q-card class="shadow-1 bg-card-body text-card-body">
+              <q-card-section>
+                <div class="text-caption text-grey-7 q-mb-xs">
+                  <q-icon name="category" class="q-mr-xs" /> Kategorien
+                </div>
+                <div>
+                  <q-badge v-for="cat in recipe.categories" :key="cat.id" color="secondary" class="q-mr-xs q-mb-xs">
+                    {{ cat.name }}
+                  </q-badge>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+
+        <q-card class="shadow-2 bg-card-body text-card-body q-mb-md">
+          <q-card-section>
+            <div class="row q-col-gutter-md">
+              <div class="col-auto">
+                <div class="text-caption text-grey-7">
+                  <q-icon name="update" class="q-mr-xs" /> {{ $t('page.recipe_details.last_update') || 'Letzte Änderung' }}:
+                  {{ new Date(recipe.lastUpdate).toLocaleDateString() }}
+                </div>
+              </div>
+              <div class="col-auto">
+                <div class="text-caption text-grey-7">
+                  <q-icon name="person" class="q-mr-xs" /> {{ $t('page.recipe_details.by') }} {{ recipe.owner?.username || 'Unbekannt' }}
+                </div>
+              </div>
+              <div class="col-auto">
+                <div class="text-caption text-grey-7">
+                  <q-icon name="info" class="q-mr-xs" /> ID: {{ recipe.id }}
+                </div>
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
       </div>
     </div>
+    <!-- Dialoge -->
     <c-question
       v-model:show="deleteDialog"
       :loading="deleting"
