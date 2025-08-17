fae019161b057397a860fecc8f58c3e469b0387d
alex9849 <alexander@liggesmeyer.net>
2025-08-17 19:12:53 +0200

    Added normal name as attribute

--- backend/src/main/resources/db/migration/V1.8.0__Recipe_Features.sql
+++ backend/src/main/resources/db/migration/V1.8.0__Recipe_Features.sql
@@ -1,11 +1,14 @@
 ALTER TABLE recipes
     ADD COLUMN normal_name TEXT NOT NULL default '';
-UPDATE recipes SET normal_name = name;
+UPDATE recipes
+SET normal_name = name;
 
 ALTER TABLE collections
     ADD COLUMN normal_name TEXT NOT NULL default '';
-UPDATE collections SET normal_name = name;
+UPDATE collections
+SET normal_name = name;
 
 ALTER TABLE ingredients
     ADD COLUMN normal_name TEXT NOT NULL default '';
-UPDATE ingredients SET normal_name = name;
+UPDATE ingredients
+SET normal_name = name;
